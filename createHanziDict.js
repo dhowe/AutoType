6ac42d445a3cc3846a1b2f0a104b5916bd8111c1
dhowe <daniel@rednoise.org>
2018-12-03 21:39:41 +0800

    ready for sub-char parts

---
+++ createHanziDict.js
@@ -0,0 +1,57 @@
+const DICT = "data/dictionary.txt";
+const STROKES = "data/graphics.txt";
+const OUTPUT = "chardata.json";
+
+var fs = require("fs"), chars = {};
+parseDict(fs.readFileSync(DICT, 'utf8').split('\n'));
+parseStrokes(fs.readFileSync(STROKES, 'utf8').split('\n'));
+var json = JSON.stringify(chars, null, 2);
+fs.writeFileSync(OUTPUT, json);
+console.log("Wrote JSON to "+OUTPUT);
+
+////////////////////////////////////////////////////////////////////////////////
+
+function parseDict(lines) {
+
+  function addData(chars, data) {
+    chars[data.character] = {
+      matches: data.matches,
+      character: data.character,
+      decomposition: data.decomposition
+    };
+  }
+  var count = 0;
+
+  lines.forEach(line => {
+    if (line) {
+      var data = JSON.parse(line);
+      var dcom = data.decomposition;
+      // single left/right or top/bottom pair
+      if (dcom.length == 3) {
+        if (dcom[0] === '⿰' || dcom[0] === '⿱') {
+          //console.log(data.character);//+": '"+data)
+          addData(chars, data);
+          count++;
+        }
+      }
+    }
+  });
+  console.log("Processed "+count+" characters");
+}
+
+function parseStrokes(lines, saveAsJSON) {
+
+  var count = 0;
+  lines.forEach(line => {
+    if (line) {
+      var data = JSON.parse(line);
+      if (chars.hasOwnProperty(data.character)) {
+          if (chars[data.character].hasOwnProperty('strokes'))
+            console.error("Dup. stroke data for: "+data.character);
+          chars[data.character].strokes = data.strokes;
+          count++;
+      }
+    }
+  });
+  console.log("Processed "+count+" paths");
+}
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,13 +1,11 @@
-// http://localhost/git/autochar/p5/index.html
+// http://localhost/git/AutoSave/p5/index.html
 
-var dlines, plines, ts = 0, load = true;
-var char, chars = { '拑': { character: '拑', strokes: ["M 322 533 Q 391 558 393 559 Q 402 568 396 576 Q 389 588 358 592 Q 340 595 324 590 L 273 570 Q 195 543 130 531 Q 93 521 122 507 Q 158 491 232 508 Q 250 514 273 519 L 322 533 Z", "M 271 328 Q 270 243 269 133 Q 269 111 256 101 Q 250 98 177 111 Q 144 123 147 113 Q 148 106 168 91 Q 234 42 251 12 Q 270 -22 286 -23 Q 301 -24 315 11 Q 333 57 330 130 Q 323 248 321 367 L 321 400 Q 320 467 322 533 L 324 590 Q 324 678 342 741 Q 360 771 304 793 Q 270 812 251 805 Q 233 798 249 775 Q 271 748 272 714 Q 273 698 273 570 L 273 519 Q 272 456 271 370 L 271 328 Z", "M 271 370 Q 189 324 117 277 Q 83 256 59 249 Q 47 243 45 235 Q 44 225 59 212 Q 78 196 102 189 Q 114 186 138 208 Q 196 268 271 328 L 321 367 Q 334 379 349 389 Q 368 402 377 416 Q 383 429 372 428 Q 366 428 321 400 L 271 370 Z", "M 783 429 Q 829 435 888 438 Q 948 442 954 451 Q 958 464 941 477 Q 880 516 810 492 Q 797 489 783 484 L 732 471 Q 665 459 539 428 L 491 419 Q 448 410 399 400 Q 377 396 395 380 Q 429 353 475 367 Q 481 370 491 371 L 539 385 Q 627 412 731 423 L 783 429 Z", "M 539 428 Q 539 509 549 551 Q 556 564 553 576 Q 547 585 497 613 Q 476 626 458 613 Q 454 609 460 595 Q 482 564 484 533 Q 488 487 491 433 Q 491 427 491 419 L 491 371 Q 494 212 488 168 Q 473 108 499 54 Q 505 39 512 36 Q 518 32 524 38 Q 539 47 544 87 L 544 123 Q 543 157 541 261 L 540 293 Q 539 335 539 385 L 539 428 Z", "M 672 103 Q 706 54 712 29 Q 719 -4 732 -9 Q 747 -12 765 22 Q 789 67 794 142 Q 784 286 783 429 L 783 484 Q 783 628 803 697 Q 819 727 764 749 Q 730 768 711 761 Q 693 754 709 731 Q 731 704 733 670 Q 734 651 732 471 L 731 423 Q 730 314 727 150 Q 726 129 715 121 Q 712 121 693 126 C 663 130 655 128 672 103 Z", "M 541 261 Q 607 273 655 281 Q 680 287 670 300 Q 657 315 630 317 Q 587 320 540 293 C 514 278 511 256 541 261 Z", "M 544 87 Q 553 96 672 103 C 702 105 715 106 693 126 Q 686 133 670 143 Q 654 152 623 143 Q 580 131 544 123 C 515 116 518 73 544 87 Z"] } };
+var word, charData, wordData, memory = [], memorySize = 10;
 
 function preload() {
-  if (load) {
-    dlines = loadStrings(DICT);
-    plines = loadStrings(STROKES);
-  }
+
+  charData = loadJSON('../chardata.json');
+  wordData = loadJSON('../wordlist.json');
 }
 
 function setup() {
@@ -16,49 +14,146 @@ function setup() {
   textAlign(CENTER);
   textSize(18);
 
-  if (load) {
-    chars = {};
-    parseDict(dlines);
-    parseStrokes(plines);
-  }
+  word = randWord();
+  remember(word.literal);
+
+  console.log("start: ",word);
+
+  next = findNext();
+  console.log("next", next);
 }
 
-var partIdx = 0;
+function remember(o) {
+  memory.push(o);
+  if (memory.length > memorySize)
+    memory.shift();
+}
+
+function findNext() {
+
+  // for a 2-char line AB CD, we want the letter that can be swapped to create a new word
+  var cobj0 = word.characters[0];
+  var cobj1 = word.characters[1];
+  var char0 = cobj0.character;
+  var char1 = cobj1.character;
+  var dcmp0 = cobj0.decomposition[0];
+  var dcmp1 = cobj1.decomposition[0];
+
+  console.log(dcmp0,dcmp1);
+
+  // check wordlist for entries with same first letter
+  var list = Object.keys(wordData);
+  var keep0 = [], keep1 = [], test;
+  for (var i = 0; i < list.length; i++) {
+
+    // ignore exact matches
+    if (list[i] === word.literal) {
+      console.log("SKIP: "+word.literal);
+      continue;
+    }
+
+    // replace second char if decomp matches current 2nd
+    if (list[i][0] === char0) {
+      test = charData[list[i][1]];
+      if (test.decomposition[0] === dcmp1) {
+
+        // NEXT: look at both dcmp[1]/dcmp[2]
+        // first-pass only add if either match
+        keep0.push(list[i]);
+      }
+    }
+
+    // replace first char if decomp matches current 1st
+    if (list[i][1] === char1) {
+      test = charData[list[i][0]];
+      if (test.decomposition[0] === dcmp0) {
+        // same here
+        keep1.push(list[i]);
+      }
+    }
+  }
+
+  var idx = Math.random() * (keep0.length+keep1.length) << 0;
+
+  console.log("keep0",keep0);
+  console.log("keep1",keep1);
+  //console.log("idx",idx);
+
+  if (idx < keep0.length) {
+    next = getWord(keep0[idx]);
+    next.replaceChar = 1;
+    next.replacePart = 1; // TODO
+  }
+  else {
+    next = getWord(keep1[idx-keep0.length]);
+    next.replaceChar = 0;
+    next.replacePart = 0; // TODO
+  }
+
+  return next;
+}
 
 function drawX() {
 
+  noLoop();
   background(245);
-  if (millis() - ts > 1000 && !mouseIsPressed) {
-    char = randomChar(chars);
-    ts = millis();
-  }
-
-  if (mouseX < width/2) partIdx = 0;
-  if (mouseX >= width/2) partIdx = 1;
-  if (mouseX >= width) partIdx = -1;
-
-  renderCharacters([randomProp(chars), randomProp(chars)]);
-  //renderPath(char, { part: partIdx });
-  //text(char.definition, width / 2, height - 10);
-  noloop();
+  renderWord(word);
 }
 
-function randomWord() {
-
-}
-
-function renderCharacters(chars) {
-  var s = "";
-  for (var i = 0; i < chars.length; i++)
-    s += chars[i].character;
-  console.log("renderCharacters: "+s);
-  for (var i = 0; i < chars.length; i++) {
-    renderPath(chars[i], i, -1);
+function renderWord(word) {
+  for (var i = 0; i < word.characters.length; i++) {
+    renderPath(word.characters[i], i);
   }
 }
+
+function getWord(literal) {
+  var chrs = [];
+  for (var i = 0; i < literal.length; i++) {
+    if (!charData.hasOwnProperty(literal[i])) {
+      throw Error('randWord() failed: '+literal+" "+i+": "+literal[i]);
+    }
+    chrs.push(charData[literal[i]]);
+  }
+  return { literal: literal, characters: chrs };
+}
+
+function randWord() {
+  return getWord(randKey(wordData));
+}
+
+function randVal(obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[keys.length * Math.random() << 0]];
+}
+
+function randKey(obj) {
+    var keys = Object.keys(obj);
+    return keys[keys.length * Math.random() << 0];
+}
+
+// var partIdx = 0;
+//
+// function drawX() {
+//
+//   background(245);
+//   if (millis() - ts > 1000 && !mouseIsPressed) {
+//     char = randomChar(charData);
+//     ts = millis();
+//   }
+//
+//   if (mouseX < width/2) partIdx = 0;
+//   if (mouseX >= width/2) partIdx = 1;
+//   if (mouseX >= width) partIdx = -1;
+//
+//   renderCharacters([randomProp(charData), randomProp(charData)]);
+//   //renderPath(char, { part: partIdx });
+//   //text(char.definition, width / 2, height - 10);
+//   noloop();
+// }
 
 function renderPath(char, charPos, options) {
 
+  //console.log('renderPath', char);
   var pg = options && options.renderer || this._renderer;
   if (typeof pg === 'undefined') throw Error('No renderer');
   if (typeof char.matches === 'undefined') throw Error('No matches: '+char.character);
